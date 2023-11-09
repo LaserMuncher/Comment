cf329c8d7a23420abff6d2aea6c063b0df7707f5
Arjay Ranzel Z. Ramirez <143532677+LaserMuncher@users.noreply.github.com>
2023-11-09 18:18:17 +0800

    Add files via upload

---
+++ Post/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include, re_path  # para mainclude ung views ng tweets app
+from django.conf import settings
+from django.conf.urls.static import static
+
+from comment.views import comment_post
+from . import views
+from .views import *
+from templates import *
+
+
+urlpatterns = [
+    path('profile/<str:username>/<int:post_id>/like/', like, name='like'),
+    path('get-likes/<str:username>/<int:post_id>/', views.get_likes, name='get_likes'),
+    path('profile/<str:username>/<int:post_id>/likedby', likedby, name='likedby'),
+    path('profile/<str:username>/<int:post_id>/delete/', delete_post, name='delete_post'),
+    path('comment_post/<int:post_id>/', comment_post, name='comment_post'),
+]
---
+++ Post/views.py
@@ -0,0 +1,132 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse, HttpResponseForbidden, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from accounts.models import UserAccount
+from .models import Post, UserLike
+from django.urls import reverse_lazy
+from django import forms
+from Post.forms import PostForm
+from django.contrib import messages
+
+
+@login_required
+def create_post(request, username):
+    if request.user.username != username:
+        raise Http404("You are not allowed to create a post on this profile.")
+
+    if request.method == 'POST':
+        form = PostForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            user = request.user
+            print('shet')
+            print(request.POST)
+            post = form.save(commit=False)
+            post.user = user
+            post.save()
+
+            return redirect('profile', username=username)
+    else:
+        form = PostForm()
+
+    return render(request, 'posts/post.html', {'form': form})
+
+
+@login_required
+def post_detail(request, username, post_id):
+    user = get_object_or_404(UserAccount, username=username)
+    post = get_object_or_404(Post, pk=post_id, user=user)
+
+    user_like = None
+    if request.user.is_authenticated:
+        try:
+            user_like = UserLike.objects.get(voter=request.user, post=post)
+        except UserLike.DoesNotExist:
+            user_like = None
+
+    return render(request, 'Posts/posts_detail.html', {
+        'user': user,
+        'post': post,
+        'user_like': user_like,
+    })
+
+
+@login_required
+def like(request, username, post_id):
+    user = request.user
+    target_post = Post.objects.get(pk=post_id)
+
+    try:
+        user_like, created = UserLike.objects.get_or_create(voter=user, post=target_post)
+
+        if created:
+            target_post.likes += 1
+            target_post.save()
+            target_post.likes.add(user)
+        elif not user_like.is_liked:
+            user_like.is_liked = True
+            user_like.save()
+            target_post.likes += 1
+            target_post.save()
+            target_post.liked_by.add(user)
+        else:
+            user_like.is_liked = False
+            user_like.save()
+            target_post.likes -= 1
+            target_post.save()
+            target_post.liked_by.remove(user)
+
+        return JsonResponse({'likes': target_post.likes})
+    except Post.DoesNotExist:
+        return JsonResponse({'error': 'The post does not exist.'}, status=404)
+
+
+@login_required
+def get_likes(request, post_id, username):
+    post = get_object_or_404(Post, pk=post_id)
+    liked_users = [user.username for user in post.liked_by.all()]
+    return JsonResponse({'liked_users': liked_users})
+
+
+@login_required
+def likedby(request, post_id, username):
+    post = Post.objects.get(id=post_id)
+    liked_users = UserLike.objects.filter(post=post, is_liked=True)
+    return render(request, 'posts/liked_by.html', {'post': post, 'liked_users': liked_users})
+
+
+@login_required
+def delete_post(request, username, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+        if request.user == post.user:
+            # Only allow deletion if the logged-in user is the owner of the post
+            post.delete()
+            return redirect('profile', username=username)
+        else:
+            # If the logged-in user is not the owner, return a forbidden response
+            return HttpResponseForbidden("You are not allowed to delete this post.")
+    except Post.DoesNotExist:
+        pass
+
+    return redirect('profile', username=username)
+
+
+@login_required
+def customization(request, username):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = request.user
+
+            post = form.save(commit=False)
+            post.user = user
+            post.save()
+
+            return redirect('profile', username=username)
+    else:
+        form = PostForm()
+
+    return render(request, 'customization/customization.html', {'form': form})
